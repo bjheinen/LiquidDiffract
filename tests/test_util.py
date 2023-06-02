4cf75b19cc58ff7db0a579875b3eb2fb64fcf542
bjheinen <benedict.heinen@gmail.com>
2023-06-02 19:13:52 +0100

    Test custom test class for ld unit tests

---
+++ tests/test_util.py
@@ -0,0 +1,30 @@
+import unittest
+import os.path
+import numpy as np
+from util import CustomAssertions
+from util import data_path
+
+class TestCustomAssertions(unittest.TestCase, CustomAssertions):
+    def test_assert_float_equal(self):
+        self.assertFloatEqual(1000.0, 1001, rtol=1e-3, atol=1e-16)
+        self.assertFloatEqual(1000.0, 1000.0+1e-5)
+        self.assertFloatEqual(1e-5, 1e-5+1e-12)
+        self.assertFloatEqual(0, 1e-17)
+
+    def test_assert_array_equal(self):
+        a = np.array([1,1e-12,2,1e-5,1e6,0])
+        b = np.array([1,1e-12,2,1e-5,1e6,0])
+        self.assertArrayEqual(a, b)
+
+    def test_assert_float_array_equal(self):
+        a = np.array([1,1e-12,2,1e-5,1e6,0])
+        b = a + 1e-20
+        self.assertFloatArrayEqual(a,b)
+
+class TestDataPath(unittest.TestCase):
+    def test_data_path(self):
+        fname = os.path.join(data_path, 'atomic_ff_Ga_0-12.npy')
+        assert os.path.isfile(fname)
+
+if __name__ == "__main__":
+    unittest.main()
