9e824d0810565913a2d11248e481a7f24516a1f6
bjheinen <benedict.heinen@gmail.com>
2023-07-10 23:52:42 +0100

    Warn if version number like 1.1.1-dev

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import unittest
+import warnings
 
 class TestVersionFile(unittest.TestCase):
     def test_appname(self):
@@ -9,9 +10,10 @@ class TestVersionFile(unittest.TestCase):
     def test_version(self):
         from LiquidDiffract.version import __version__
         # Version numbers on dev branch like '1.2.7-dev'
-        # Check if version number still -dev
-        self.assertNotIn('dev', __version__)
-        self.assertTrue(__version__[-1].isdigit())
+        # Check if version number still -dev and warn if true
+        self.assertGreater(len(__version__), 5)
+        if 'dev' in __version__ or not __version__[-1].isdigit():
+            warnings.warn(f'Version number not in release format| v: {__version__}')
 
 if __name__ == "__main__":
     unittest.main()
