93c16d883c269b9477ba231cf709764536c874f7
bjheinen <benedict.heinen@gmail.com>
2024-05-10 15:12:45 +0100

    Add qtpy to install_requires

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ version = {}
 with open(path.join(here, 'LiquidDiffract/version.py')) as fp:
     exec(fp.read(), version)
 
-
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
@@ -26,7 +25,8 @@ setup(
     packages=find_packages(),
     python_requires='>=3.9',
     install_requires=['numpy>=1.20.0', 'scipy>=1.6.0', 'PyQt6>=6.2.0',
-                      'pyqtgraph>=0.13.0', 'lmfit>=1.0.2', 'packaging>=14.1'],
+                      'qtpy>=2.3.0', 'pyqtgraph>=0.13.0', 'lmfit>=1.0.2',
+                      'packaging>=14.1'],
     package_data={'LiquidDiffract': ['resources/*', 'resources/icons/*',
                                      'resources/hubbel_compton/*',
                                      'resources/docs/*',
